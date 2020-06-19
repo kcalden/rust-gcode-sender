ef074718c4f23f623fd65866474e5d3cada6156d
Karl Calden <kcalden323@gmail.com>
2020-06-19 00:52:55 -0400

    Serial port list example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod hwinterface;
+mod serial_interface;
+
+use hwinterface::HWInterface;
+use serial_interface::SerialInterface;
+
 fn main() {
-    println!("Hello, world!");
+    let port_list = SerialInterface::list();
+
+    for port in port_list.iter() {
+        println!("{}", port);
+    }
 }
