c6b40a1091de617b3bc0c93e37014af40671032d
Karl Calden <kcalden323@gmail.com>
2020-06-22 13:21:36 -0400

    Update example

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod hwinterface;
+mod machine;
 use hwinterface::*;
-use serial_interface::{SerialInterface, PortSettings};
+use serial_interface::{SerialInterface};
 
 fn main() {
-    let serial_port_settings = SerialInterface::default_settings();
-    println!("COM Port -> {}", serial_port_settings.port_name);
-    println!("Baudrate -> {}", serial_port_settings.serial_settings.baud_rate);
-
-    let port_list = SerialInterface::list();
+    let new_serial_interface: SerialInterface = Default::default();
+    println!("COM Port -> {}", new_serial_interface.settings.port_name);
+    println!("Baudrate -> {}", new_serial_interface.settings.serial_settings.baud_rate);
+    
+    let port_list = new_serial_interface.list();
     for port in port_list.iter() {
         println!("{}", port);
     }
