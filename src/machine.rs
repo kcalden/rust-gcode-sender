0a6ee618fcf50291e3b62f5fe82b65b328295e93
Karl Calden <kcalden323@gmail.com>
2020-06-23 01:34:16 -0400

    Add load_from_file() to Machine structure

--- src/machine.rs
+++ src/machine.rs
@@ -1,9 +1,16 @@
 use crate::hwinterface;
+use crate::parser;
+use crate::util;
 
 type HWInterface = Box<dyn hwinterface::HardwareInterface>;
 
+enum MsgType {
+    Normal(String),
+    Error(String),
+}
+
 /// Contains information aobut the machine
-struct Machine {
+pub struct Machine {
     /// Unique name of the machine
     name: String,
 
@@ -13,7 +20,7 @@ struct Machine {
     /// Lines of the program
     /// 
     /// TODO: Create enums to describe each line
-    program: Vec<String>,
+    program: Vec<parser::Command>,
 
     /// Current line to be sent to the machine
     program_index: u32,
@@ -22,5 +29,18 @@ struct Machine {
     interface: Option<HWInterface>,
 
     /// Communication history
-    comm_history: Vec<String>,
+    comm_history: Vec<MsgType>,
+}
+
+impl Machine {
+    fn load_from_file(&mut self, file_path: &str) {
+        if let Some(contents) = util::load_file(file_path) {
+            self.program = parser::parse_file(&contents);
+            self.comm_history.push(MsgType::Normal(
+                String::from(format!("Loaded file {}", file_path))));
+        }else{
+            self.comm_history.push(MsgType::Error(
+                String::from(format!("Failed to load file {}", file_path))));
+        }
+    }
 }
