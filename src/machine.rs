9c1be33017905a0441f49f460fa25f348419c2a3
Karl Calden <kcalden323@gmail.com>
2020-06-23 00:46:11 -0400

    Comment Machine structure

--- src/machine.rs
+++ src/machine.rs
@@ -2,9 +2,25 @@ use crate::hwinterface;
 
 type HWInterface = Box<dyn hwinterface::HardwareInterface>;
 
+/// Contains information aobut the machine
 struct Machine {
+    /// Unique name of the machine
+    name: String,
+
+    /// Path to the loaded GCode file
     filepath: String,
+
+    /// Lines of the program
+    /// 
+    /// TODO: Create enums to describe each line
     program: Vec<String>,
+
+    /// Current line to be sent to the machine
+    program_index: u32,
+
+    /// Communication interface
     interface: Option<HWInterface>,
+
+    /// Communication history
     comm_history: Vec<String>,
 }
