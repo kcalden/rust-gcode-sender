60835dd8656c52219a464d7d280afbb0a60826bf
Karl Calden <kcalden323@gmail.com>
2020-07-01 23:54:57 -0400

    Canonicalize path in file load

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,10 @@
 pub fn load_file(file_path: &str) -> Option<String> {
-    if let Ok(contents) = std::fs::read_to_string(file_path) {
+    let resolved_path = match std::fs::canonicalize(file_path) {
+        Ok(path) => path,
+        _ => return None
+    };
+    
+    if let Ok(contents) = std::fs::read_to_string(resolved_path) {
         return Some(contents);
     }
     None
