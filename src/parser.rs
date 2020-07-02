538feeb11b109906a4e3f39dd979f2bbb741f9c6
Karl Calden <kcalden323@gmail.com>
2020-07-01 21:27:37 -0400

    Use parallel iterator for parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,62 +1,64 @@
 use gcode;
 use regex::Regex;
+use rayon::prelude::*;
+use std::time::Instant;
 
 pub enum Command {
-    MoveGCode(gcode::GCode),
-    ParsedGCode(gcode::GCode),
-    GenericGCode(String),
-    HostCommand(String)
-
+    Move(gcode::GCode),
+    Generic(gcode::GCode),
+    Home(String),
+    HostCommand(String),
+    Nothing,
 }
 
-/// Parse file line by line
 pub fn parse(file_contents: &str) -> Vec<Command> {
-    // let file_contents = match std::fs::read_to_string(file_path) {
-        //     Ok(contents) => contents,
-        //     _ => return Vec::new()
-        // };
-        
+    let lines:Vec<&str> = file_contents.lines()
+    .filter(|src_line| {
+        // Return host commands
+        if src_line.starts_with(";@") {
+            return true;
+        }
+        // Skip pure space or comments
+        if src_line.len() == 0 || src_line.starts_with(";") {
+            return false;
+        }else{
+            return true;
+        }
+    }).collect();
+
     let regex_home_flags = Regex::new(r"^\s*(G28(\s+(X|Y|Z))*)").unwrap();
-    let mut commands = Vec::new();
-    
-    // Here I'm just going to do my own parsing before passing it to the gcode
-    for src_line in file_contents.lines() {
+
+    let parse_iter = lines.par_iter()
+    .map(|src_line| {
         let src_line = src_line.trim();
-        
-        // Skip pure space
-        if src_line.len() == 0 { continue }
-        
         // Matches host commands
         if src_line.starts_with(";@"){
             let hcmd = src_line.replace(";@", "");
-            commands.push(Command::HostCommand(hcmd));
+            return Command::HostCommand(hcmd);
         }
         
-        // Skip comment if we haven't matched a host command
-        if src_line.starts_with(";") {continue}
-
         // Handle special case of G28 with flags (3D printer firmwares)
         if src_line.starts_with("G28") {
             if let Some(caps) = regex_home_flags.captures(src_line){
-                commands.push(Command::GenericGCode(String::from(&caps[1])))
+                return Command::Home(String::from(&caps[1]));
             }
         }
-
+        
         // Now, we can start parsing
         if let Some(gc) = gcode::parse(src_line).next() {
             if let gcode::Mnemonic::General = gc.mnemonic() {
                 match gc.major_number() {
                     0 | 1 => {
-                        commands.push(Command::MoveGCode(gc));
-                        continue
+                        return Command::Move(gc);
                     },
                     _ => ()
                 }
             }
-            commands.push(Command::ParsedGCode(gc));
-            continue
+            return Command::Generic(gc);
+        }else {
+            return Command::Nothing;
         }
-    }
+    });
 
-    commands
+    parse_iter.collect()
 }
